4e3ca5f35396bc8bdef5856c6bb9f64ca6d8d77a
zetef <dediustef@gmail.com>
2019-04-22 12:02:37 +0300

    small adjustments

--- include/constants.h
+++ include/constants.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define SCREEN_WIDTH 600
+#define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
 #define DEFAULT_DRIVER -1
 #define SCREEN_FPS 60                          //astea pentru cand voi face un timer
